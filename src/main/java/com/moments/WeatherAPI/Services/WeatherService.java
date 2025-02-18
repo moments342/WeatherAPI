d30dc30f5685a915dc0d5b0fecbf95d5daa81702
podei <gabriellemosfran@gmail.com>
2025-02-17 23:51:01 -0400

    Weather Service and Controller 1.0 created (future modifications needed)

--- src/main/java/com/moments/WeatherAPI/Services/WeatherService.java
+++ src/main/java/com/moments/WeatherAPI/Services/WeatherService.java
@@ -1,5 +1,29 @@
 package com.moments.WeatherAPI.Services;
 
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Service
 public class WeatherService {
 
+    private final WebClient webClient;
+
+    public WeatherService() {
+        this.webClient = WebClient.builder()
+                .baseUrl("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline")
+                .build();
+    }
+
+    @Cacheable("weatherData")
+    public String getWeather(String city) {
+        return this.webClient.get()
+                .uri(uriBuilder -> uriBuilder.path("/{city}")
+                        .queryParam("key", "EXLKSZUA8L9GZFEVKY45CLQRA")
+                        .build(city))
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+    }
 }
+
